925a484b9fba75cbaa18c1b6742687d3eecd85e2
Kayden <ghost@gmail.com>
2020-11-19 16:01:23 +0800

    Started project.

---
+++ settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 15:49:43 2020
+
+@author: ghost
+
+work: settings
+"""
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+    
+    def __init__(self):
+        """Initailize the game's settings"""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 500
+        self.bg_color = (230,230,230)
